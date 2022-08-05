71e0808a4c75c47fc8760a84ea3c2525e63c2f2c
Sahan Marapana <contactsahan@gmail.com>
2022-08-06 03:25:30 +0530

    test

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,6 +1,11 @@
+output "private_ip" {
+description = "Private IP of the web server"
+value       = aws_instance.web.private_ip
+}
+
 resource "local_file" "ansible_inventory" {
   content = templatefile("inventory.tmpl", {
-    webserver_ip = data.aws_instances.web.private_ips[0],
+    webserver_ip = "${aws_instance.web.private_ip}"
   })
   filename = "inventory"
 }
