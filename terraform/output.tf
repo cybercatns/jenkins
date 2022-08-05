1ef30659ec067da9f2722dc9d337f535ce2b4b0d
Sahan Marapana <contactsahan@gmail.com>
2022-08-06 02:18:43 +0530

    test

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,6 +1,6 @@
 resource "local_file" "ansible_inventory" {
   content = templatefile("inventory.tmpl", {
-    webserver_ip = data.aws_instances.test.private_ips,
+    webserver_ip = data.aws_instances.web.private_ips,
   })
   filename = "inventory"
 }
