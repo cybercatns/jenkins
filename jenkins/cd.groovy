b28d88b3cc40a22263da875ceb67a9e5b3469cc9
Sahan Marapana <contactsahan@gmail.com>
2022-08-06 02:02:40 +0530

    test

--- jenkins/cd.groovy
+++ jenkins/cd.groovy
@@ -1,1 +1,4 @@
 hi
+
+terraform init
+terraform apply -auto-approve
--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,7 @@ terraform {
 resource "aws_instance" "web" {
   ami           = "ami-0ff89c4ce7de192ea"
   instance_type = "t3.micro"
+  key_name = "Jenkins"
 
   tags = {
     Name = "HelloWorld"
