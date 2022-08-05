4fe66430b94c089f685cc00c778a5ea9b9853e00
Sahan Marapana <contactsahan@gmail.com>
2022-08-06 02:25:28 +0530

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,13 @@ terraform {
   }
 }
 
+data "aws_instances" "web" {
+  instance_tags = {
+    Name = "WebServer"
+  }
+  instance_state_names = ["running"]
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-0ff89c4ce7de192ea"
   instance_type = "t3.micro"
